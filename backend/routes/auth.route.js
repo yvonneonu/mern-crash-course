507cde1c9daa6a66d2ff61fc60da4cc9af87b40f
yvonneonu <yvonneonuorah@gmail.com>
2025-02-19 16:43:46 +0100

    APP

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -29,7 +29,7 @@ router.get(
       });
       console.log(token);
 
-      res.redirect(`${process.env.FRONTEND_URL}?token=${token}`);
+      res.redirect(`/`);
     } catch (error) {
       console.error(error);
       res.status(500).send("Internal Server Error");
